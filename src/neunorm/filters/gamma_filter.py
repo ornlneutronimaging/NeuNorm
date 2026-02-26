Fix propagation of variance in gamma filter

src/neunorm/filters/gamma_filter.py
@@ -81,8 +81,9 @@ def apply_gamma_filter(
 
     # Identify outliers
     outlier_mask = values > local_threshold
+    outlier_count = np.sum(outlier_mask)
 
-    logger.debug("Identified {} outliers in data of shape {}", outlier_mask.sum(), values.shape)
+    logger.debug("Identified {} outliers in data of shape {}", outlier_count, values.shape)
 
     # Replace outliers with local median
     filtered_values = values.copy()
@@ -92,10 +93,32 @@ def apply_gamma_filter(
     input_variances = data.data.variances
     filtered_variances = input_variances.copy() if input_variances is not None else None
 
-    if not preserve_variance and input_variances is not None:
-        # Local variance estimate from existing per-pixel variances
-        local_var = ndi.median_filter(filtered_variances, footprint=footprint, mode="nearest")
-        filtered_variances[outlier_mask] = local_var[outlier_mask]
+    if not preserve_variance and input_variances is not None and outlier_count > 0:
+        rng = np.random.default_rng()
+        # recalculate variance for outliers from local neighborhood.
+        # This is an approximation using Monte Carlo sampling of the local neighborhood
+        # to estimate the variance of the median.
+        for idx in np.ndindex(outlier_mask.shape):
+            if outlier_mask[idx]:
+                # get list of neighboring values
+                neighbor_indices = tuple(
+                    slice(max(0, i - size[d] // 2), min(s, i + size[d] // 2 + 1))
+                    for d, (i, s) in enumerate(zip(idx, values.shape))
+                )
+                neighbor_values = values[neighbor_indices].flatten()
+                mid_point = len(neighbor_values) // 2
+                neighbor_values = np.delete(neighbor_values, mid_point)  # remove center pixel
+                neighbor_variances = np.delete(
+                    input_variances[neighbor_indices].flatten(), mid_point
+                )  # remove center pixel variance
+                # Generate random samples based on neighboring values and variances
+                samples = rng.normal(
+                    loc=neighbor_values, scale=np.sqrt(neighbor_variances), size=(100_000, len(neighbor_values))
+                )
+                median_samples = np.median(samples, axis=1)
+                # Update variance for this pixel to the variance of the median samples
+                filtered_variances[idx] = np.var(median_samples, ddof=1)
+                logger.debug("Updating variance for outlier at index {} to {}", idx, filtered_variances[idx])
 
     out = data.copy(deep=False)
     out.data = sc.array(

tests/unit/test_gamma_filter.py
@@ -12,6 +12,7 @@ def test_apply_gamma_filter_3d():
 
     # Create simple sample data
     sample_data = np.full((10, 5, 5), 10.0)
+    sample_data[5][1] = 11.0  # Add some small difference so that the std is non-zero
     sample_data[5, 2, 2] = 100.0  # Add a gamma spike, should be replaced by median of 10
 
     sample = sc.DataArray(
@@ -24,11 +25,18 @@ def test_apply_gamma_filter_3d():
     # Should still be in counts
     assert corrected.unit == sc.units.counts
 
-    # Values should be 10
-    np.testing.assert_allclose(corrected.values, 10.0)
+    # Values should be 10 except for one row of 11. But the 100 should be replaced by 10.
+    expected_values = np.full((10, 5, 5), 10.0)
+    expected_values[5][1] = 11.0
+    np.testing.assert_allclose(corrected.values, expected_values)
 
     # Variance should be updated to local median variance.
-    np.testing.assert_allclose(corrected.variances, 10.0)
+    expected_variance = np.full((10, 5, 5), 10.0)
+    expected_variance[5][1] = 11.0  # This pixel is not an outlier, so variance should be unchanged.
+    # This pixel is an outlier. Variance will be small because it is replaced by the median of multiple samples.
+    expected_variance[5, 2, 2] = 1.75
+
+    np.testing.assert_allclose(corrected.variances, expected_variance, rtol=0.02)
 
 
 def test_apply_gamma_filter_high_threshold():
@@ -101,4 +109,7 @@ def test_apply_gamma_filter_basic_2d():
     np.testing.assert_allclose(corrected.values, 10.0)
 
     # Variance should be propagated.
-    np.testing.assert_allclose(corrected.variances, 10.0)
+    expected_variance = np.full((5, 5), 10.0)
+    # This pixel is an outlier. Variance will be small because it is replaced by the median of multiple samples
+    expected_variance[2, 2] = 1.68
+    np.testing.assert_allclose(corrected.variances, expected_variance, rtol=0.02)